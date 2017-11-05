36f39ce4481867fc87c8332bcda52c8343dc8c53
beno.masschelein <benoit.masschelein@hotmail.com>
2017-11-05 10:45:06 +0100

    playCard works test class extended

--- Test/src/test.java
+++ Test/src/test.java
@@ -7,20 +7,14 @@ import java.rmi.registry.Registry;
  * Created by Benoit on 4/11/17.
  */
 public class test {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         DatabaseServerMain.main(args);
         ApplicationServerMain.main(args);
 
         Registry myRegistry = null;
-        ApplicationServerController applicationServerController = null;
-        try {
-            myRegistry = LocateRegistry.getRegistry("localhost", 7280);
-            applicationServerController = (ApplicationServerController) myRegistry.lookup("ClientApplicationService");
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
+        myRegistry = LocateRegistry.getRegistry("localhost", 7280);
+        final ApplicationServerController applicationServerController = (ApplicationServerController) myRegistry.lookup("ClientApplicationService");
+
 
         long unoGameId = 0;
 
@@ -32,29 +26,51 @@ public class test {
             e.printStackTrace();
         }
 
-        ApplicationServerGameInterface applicationServerGameInterface;
-        int playerId1;
+        final int playerId1 = 0;
         int playerId2;
         int playerId3;
 
         // add three players to Unogame;
-        try {
-            applicationServerGameInterface = (ApplicationServerGameInterface) myRegistry.lookup("UnoGame" + unoGameId);
-            playerId1 = applicationServerController.joinGame(new Player("jefke"), unoGameId);
-            playerId2 = applicationServerController.joinGame(new Player("joske"), unoGameId);
-            playerId3 = applicationServerController.joinGame(new Player("axelleke"), unoGameId);
+        final ApplicationServerGameInterface applicationServerGameInterface = (ApplicationServerGameInterface) myRegistry.lookup("UnoGame" + unoGameId);
+        applicationServerController.joinGame(new Player("jefke"), unoGameId);
+        playerId2 = applicationServerController.joinGame(new Player("joske"), unoGameId);
+        playerId3 = applicationServerController.joinGame(new Player("axelleke"), unoGameId);
 
-            System.out.println(applicationServerGameInterface.startMessage(playerId1));
-            System.out.println(applicationServerGameInterface.startMessage(playerId2));
-            playerId3 = applicationServerController.joinGame(new Player("axelleke"), unoGameId);
+        System.out.println(applicationServerGameInterface.startMessage(playerId1));
+        System.out.println(applicationServerGameInterface.startMessage(playerId2));
+        System.out.println(applicationServerGameInterface.startMessage(playerId3));
+
+
+        Runnable updateGame = new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        System.out.println(applicationServerGameInterface.subscribe(playerId1));
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        new Thread(updateGame).start();
+
+        Card card1 = new NumberCard(1, 5);
+        Card card2 = new NumberCard(2, 5);
+        Card card3 = new NumberCard(3, 5);
+        Card card4 = new NumberCard(4, 5);
+
+        try {
+            System.out.println(applicationServerGameInterface.drawCard(0));
+            System.out.println(applicationServerGameInterface.playCard(0, card1));
+            System.out.println(applicationServerGameInterface.playCard(0, card2));
+            System.out.println(applicationServerGameInterface.playCard(0, card3));
+            System.out.println(applicationServerGameInterface.playCard(0, card4));
 
         } catch (RemoteException e) {
             e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
         }
-
-
 
 
     }
